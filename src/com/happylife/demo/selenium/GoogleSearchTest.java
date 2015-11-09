298926f273dad362b04fa03e1fd93a03ff6f0185
Andy Feng <andyfengc@outlook.com>
2015-11-09 17:05:17 -0500

    fix

--- src/com/happylife/demo/selenium/GoogleSearchTest.java
+++ src/com/happylife/demo/selenium/GoogleSearchTest.java
@@ -12,15 +12,22 @@ public class GoogleSearchTest {
 
 	@Test
 	public void test() throws InterruptedException {
+		// locate chrome driver
 		String chromeDriverPath = System.getProperty("user.dir") + "/lib/chromedriver.exe";
 		System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+		// create new web driver instance
 		WebDriver driver = new ChromeDriver();
+		// load google search page
 		driver.get("http://www.google.ca");
-		Thread.sleep(5000); // Let the user actually see something!
+		Thread.sleep(5000); // wait for a while so that we can see the page
+		// locate search textbox
 		WebElement searchBox = driver.findElement(By.name("q"));
+		// enter search keyword
 		searchBox.sendKeys("ChromeDriver");
+		// search now
 		searchBox.submit();
-		Thread.sleep(5000); // Let the user actually see something!
+		Thread.sleep(5000); // wait for a while so that we observe
+		// exit
 		driver.quit();
 	}
 
